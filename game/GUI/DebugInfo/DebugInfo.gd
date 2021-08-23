1c110cfc43680023cf5f2527312cc1ac5b9cd48b
Jairo <howoca2039@invql.com>
2021-08-23 15:56:58 -0300

    new town level. works at 60 fps on PCs with graphics card, at 30 +/- on PC without. Web export shows meshes inside baked light extents transparent (or with render mode added instead of mix)

--- game/GUI/DebugInfo/DebugInfo.gd
+++ game/GUI/DebugInfo/DebugInfo.gd
@@ -9,15 +9,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
 	var info = """
-	FPS: %.2f
+	FPS: %d
 	Process: %.2f ms
 	Static Memory: %.2f MB
+	Dynamic Memory: %.2f MB
+	Rendered Objects: %d
+	Rendered Vertices: %d
+	Draw Calls: %d
 	"""
-#	CPU Time: %.1f
-#	GPU Time: %.1f
 
 	$Label.text = info % [
 		Performance.get_monitor(Performance.TIME_FPS),
 		Performance.get_monitor(Performance.TIME_PROCESS),
 		OS.get_static_memory_usage() / B_TO_MB,
+		OS.get_dynamic_memory_usage() / B_TO_MB,
+		Performance.get_monitor(Performance.RENDER_OBJECTS_IN_FRAME),
+		Performance.get_monitor(Performance.RENDER_VERTICES_IN_FRAME),
+		Performance.get_monitor(Performance.RENDER_DRAW_CALLS_IN_FRAME),
 	]
